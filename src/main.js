0f0a7b4a7dbf962caa18d94b8b000df42f1904b7
PabloGibaja <Aonemaxino13>
2021-11-05 19:18:18 +0100

    testing coordinates

--- src/main.js
+++ src/main.js
@@ -7,16 +7,18 @@ function generateEarth(){
     const loader = new THREE.TextureLoader();
     const material = new THREE.MeshBasicMaterial({
         map: loader.load('../assets/1_earth_8k.jpg'),
+        //map: loader.load('../assets/8081_earthmap10k.jpg'),
+        
 
       });
     
-    geometry = new THREE.SphereGeometry(50, 512, 512); //size
+    geometry = new THREE.SphereGeometry(1, 30, 30); //size
 
     sphere = new THREE.Mesh (geometry, material);
     sphere.position.x = 0
     sphere.position.y = 0
     scene.add(sphere); 
-    console.log('Esfera generada en x: '+ sphere.position.x+' y: '+sphere.position.y)
+    console.log('Esfera generada en x: '+ sphere.position.x+' y: '+sphere.position.y+' z: '+sphere.position.z)
     
 }
 
@@ -34,11 +36,12 @@ function init(){
     document.body.appendChild(renderer.domElement);
     var controls = new THREE.OrbitControls(camera, renderer.domElement);  //CONTROLES DE USUARIO
 
-    camera.position.z = 60; // ALEJO LA CAMARA DEL CENTRO DE LA ESCENA
+    camera.position.z = 2; // ALEJO LA CAMARA DEL CENTRO DE LA ESCENA
 
     //generateCubes(5);
     //generateSpheres(5)
     generateEarth();
+    generateCities();
 
 }
 
@@ -53,8 +56,33 @@ function animate() {
     renderer.render(scene, camera);
 }
 
+
 function generateCities(){
-    //TODO
+    let city = new THREE.Mesh(
+        new THREE.SphereBufferGeometry(0.005,20,20),
+        new THREE.MeshBasicMaterial({color:0xff0000})
+    );
+
+    material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+   
+        //madrid example 
+    lon =  40.5 * Math.PI/180;
+    lat =  87 * Math.PI/180;
+
+    let x = 1.21 * Math.cos(lon)*Math.sin(lat);
+    let y = 1.21 * Math.sin(lon)*Math.sin(lat);
+    let z = 1.21 * Math.cos(lat)
+
+    const points = [];
+    points.push( new THREE.Vector3( 0, 0, 0 ) );
+    points.push( new THREE.Vector3( x, y, z ) );
+    const geometry = new THREE.BufferGeometry().setFromPoints( points );
+    const line = new THREE.Line( geometry, material );
+    scene.add( line );
+    
+    city.position.set(x/1.21,y/1.21,z/1.21)
+    scene.add(city)
+    console.log('ciudad generada en'+x+','+y+','+z)
 }
 
 function onWindowResize(){
